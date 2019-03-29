752946315fdf43f44d52524fa0333b3ca951c5f3
David Arinder <david.arinder@gmail.com>
2019-03-29 12:51:10 -0700

    weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
       console.log(body.error)
       callback('Unable to find location. Try another search.', undefined)
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
   })
 }
